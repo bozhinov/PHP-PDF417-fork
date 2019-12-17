13c14ca57051d46d6b850c15cd3312a0ec058d7f
Momchil Bozhinov <momchil@bojinov.info>
2019-12-17 15:23:26 +0200

    * Moved the encoding to the DataEncoder.

--- PDF417/DataEncoder.php
+++ PDF417/DataEncoder.php
@@ -9,8 +9,10 @@ namespace PDF417;
  */
 class DataEncoder
 {
-    private $encoders;
-    private $options;
+	private $encoders;
+	private $options;
+	private $_START_CHARACTER = 0x1fea8;
+	private $_STOP_CHARACTER  = 0x3fa29;
 
     public function __construct(array $options)
     {
@@ -24,10 +26,146 @@ class DataEncoder
         $this->options = $options;
     }
 
+	/**
+	* Encodes the given data to low level code words.
+	*/
+	public function encodeData($data)
+	{
+		$codeWords = $this->encodeECC($data);
+
+		// Arrange codewords into a rows and columns
+		$grid = array_chunk($codeWords, $this->options['columns']);
+		$rows = count($grid);
+
+		// Iterate over rows
+		$pixelGrid = [];
+		foreach ($grid as $rowNum => $row) {
+
+			$table = $rowNum % 3;
+
+			// Add starting code word
+			$rowCodes = [$this->_START_CHARACTER];
+
+			// Add left-side code word
+			$left = $this->getLeftCodeWord($rowNum, $rows);
+			$rowCodes[] = Codes::getCode($table, $left);
+
+			// Add data code words
+			foreach ($row as $word) {
+				$rowCodes[] = Codes::getCode($table, $word);
+			}
+
+			// Add right-side code word
+			$right = $this->getRightCodeWord($rowNum, $rows);
+			$rowCodes[] = Codes::getCode($table, $right);
+
+			// Add ending code word
+			$rowCodes[] = $this->_STOP_CHARACTER;
+
+			$pixelRow = [];
+			foreach ($rowCodes as $value) {
+				$bin = decbin($value);
+				$len = strlen($bin);
+				for ($i = 0; $i < $len; $i++) {
+					$pixelRow[] = (boolean) $bin[$i];
+				}
+			}
+
+			$pixelGrid[] = $pixelRow;
+		}
+		
+		return $pixelGrid;
+	}
+
+	/* Encodes data to a grid of codewords for constructing the barcode. */
+	private function encodeECC($data)
+	{
+		// Encode data to code words
+		$dataWords = $this->encode($data);
+
+		// Number of code correction words
+		$ecCount = pow(2, $this->options['securityLevel'] + 1);
+		$dataCount = count($dataWords);
+
+		// Add padding if needed
+		$padWords = $this->getPadding($dataCount, $ecCount);
+		$dataWords = array_merge($dataWords, $padWords);
+
+		// Add length specifier as the first data code word
+		// Length includes the data CWs, padding CWs and the specifier itself
+		$length = count($dataWords) + 1;
+		array_unshift($dataWords, $length);
+
+		// Compute error correction code words
+		$reedSolomon = new ReedSolomon();
+		$ecWords = $reedSolomon->compute($dataWords, $this->options['securityLevel']);
+
+		// Combine the code words and return
+		return array_merge($dataWords, $ecWords);
+	}
+
+	private function getLeftCodeWord($rowNum, $rows)
+	{
+		// Table used to encode this row
+		$tableID = $rowNum % 3;
+
+		switch($tableID) {
+			case 0:
+				$x = intval(($rows - 1) / 3);
+				break;
+			case 1:
+				$x = $this->options['securityLevel'] * 3;
+				$x += ($rows - 1) % 3;
+				break;
+			case 2:
+				$x = $this->options['columns'] - 1;
+				break;
+		}
+
+		return 30 * intval($rowNum / 3) + $x;
+	}
+
+	private function getRightCodeWord($rowNum, $rows)
+	{
+		$tableID = $rowNum % 3;
+
+		switch($tableID) {
+			case 0:
+				$x = $this->options['columns'] - 1;
+				break;
+			case 1:
+				$x = intval(($rows - 1) / 3);
+				break;
+			case 2:
+				$x = $this->options['securityLevel'] * 3;
+				$x += ($rows - 1) % 3;
+				break;
+		}
+
+		return 30 * intval($rowNum / 3) + $x;
+	}
+
+	private function getPadding($dataCount, $ecCount)
+	{
+		// Total number of data words and error correction words, additionally
+		// reserve 1 code word for the length descriptor
+		$totalCount = $dataCount + $ecCount + 1;
+		$mod = $totalCount % $this->options['columns'];
+
+		if ($mod > 0) {
+			$padCount = $this->options['columns'] - $mod;
+			$padding = array_fill(0, $padCount, 900);
+		} else {
+			$padding = [];
+		}
+
+		return $padding;
+	}
+
     /**
      * Splits the input data into chains. Then encodes each chain.
      */
-    public function encode($data)
+    private function encode($data)
     {
 		switch($this->options["hint"]){
 			case "numbers":
@@ -89,7 +227,7 @@ class DataEncoder
 		return $chains;
     }
 
-	public function getEncoder($char, $pos)
+	private function getEncoder($char, $pos)
     {
         foreach ($this->encoders as $id => $encoder) {
             if ($encoder->canEncode($char)) {
--- PDF417/PDF417.php
+++ PDF417/PDF417.php
@@ -4,10 +4,8 @@ namespace PDF417;
 
 class PDF417
 {
-	private $_START_CHARACTER = 0x1fea8;
-	private $_STOP_CHARACTER  = 0x3fa29;
 	private $options = [];
-	private $pixelGrid;
+	private $renderer;
 
 	public function __construct(array $options = [])
 	{
@@ -69,31 +67,24 @@ class PDF417
 		}
 	}
 
-	private function render()
-	{
-		return (new Renderer($this->pixelGrid, $this->options));
-	}
-
 	public function toFile(string $filename, bool $forWeb = false)
 	{
 		$ext = strtoupper(substr($filename, -3));
 		($forWeb) AND $filename = null;
 
-		$renderer = $this->render();
-
 		switch($ext)
 		{
 			case "PNG":
-				$renderer->toPNG($filename);
+				$this->renderer->toPNG($filename);
 				break;
 			case "GIF":
-				$renderer->toGIF($filename);
+				$this->renderer->toGIF($filename);
 				break;
 			case "JPG":
-				$renderer->toJPG($filename, $this->options['quality']);
+				$this->renderer->toJPG($filename, $this->options['quality']);
 				break;
 			case "SVG":
-				$content = $renderer->createSVG();
+				$content = $this->renderer->createSVG();
 				if($forWeb) {
 					header("Content-type: image/svg+xml");
 					return $content;
@@ -109,7 +100,7 @@ class PDF417
 	public function forWeb(string $ext)
 	{
 		if (strtoupper($ext) == "BASE64"){
-			return ($this->render())->toBase64();
+			return $this->renderer->toBase64();
 		} else {
 			$this->toFile($ext, true);
 		}
@@ -117,140 +108,12 @@ class PDF417
 
 	public function forPChart(\pChart\pDraw $MyPicture, $X = 0, $Y = 0)
 	{
-		($this->render())->forPChart($MyPicture->gettheImage(), $X, $Y);
+		$this->renderer->forPChart($MyPicture->gettheImage(), $X, $Y);
 	}
 
-	/**
-	* Encodes the given data to low level code words.
-	*/
 	public function encode($data)
 	{
-		$codeWords = $this->encodeData($data);
-
-		// Arrange codewords into a rows and columns
-		$grid = array_chunk($codeWords, $this->options['columns']);
-		$rows = count($grid);
-
-		// Iterate over rows
-		$this->pixelGrid = [];
-		foreach ($grid as $rowNum => $row) {
-
-			$table = $rowNum % 3;
-
-			// Add starting code word
-			$rowCodes = [$this->_START_CHARACTER];
-
-			// Add left-side code word
-			$left = $this->getLeftCodeWord($rowNum, $rows);
-			$rowCodes[] = Codes::getCode($table, $left);
-
-			// Add data code words
-			foreach ($row as $word) {
-				$rowCodes[] = Codes::getCode($table, $word);
-			}
-
-			// Add right-side code word
-			$right = $this->getRightCodeWord($rowNum, $rows);
-			$rowCodes[] = Codes::getCode($table, $right);
-
-			// Add ending code word
-			$rowCodes[] = $this->_STOP_CHARACTER;
-
-			$pixelRow = [];
-			foreach ($rowCodes as $value) {
-				$bin = decbin($value);
-				$len = strlen($bin);
-				for ($i = 0; $i < $len; $i++) {
-					$pixelRow[] = (boolean) $bin[$i];
-				}
-			}
-
-			$this->pixelGrid[] = $pixelRow;
-		}
-	}
-
-	/* Encodes data to a grid of codewords for constructing the barcode. */
-	public function encodeData($data)
-	{
-		// Encode data to code words
-		$dataWords = (new DataEncoder($this->options))->encode($data);
-
-		// Number of code correction words
-		$ecCount = pow(2, $this->options['securityLevel'] + 1);
-		$dataCount = count($dataWords);
-
-		// Add padding if needed
-		$padWords = $this->getPadding($dataCount, $ecCount);
-		$dataWords = array_merge($dataWords, $padWords);
-
-		// Add length specifier as the first data code word
-		// Length includes the data CWs, padding CWs and the specifier itself
-		$length = count($dataWords) + 1;
-		array_unshift($dataWords, $length);
-
-		// Compute error correction code words
-		$reedSolomon = new ReedSolomon();
-		$ecWords = $reedSolomon->compute($dataWords, $this->options['securityLevel']);
-
-		// Combine the code words and return
-		return array_merge($dataWords, $ecWords);
-	}
-
-	private function getLeftCodeWord($rowNum, $rows)
-	{
-		// Table used to encode this row
-		$tableID = $rowNum % 3;
-
-		switch($tableID) {
-			case 0:
-				$x = intval(($rows - 1) / 3);
-				break;
-			case 1:
-				$x = $this->options['securityLevel'] * 3;
-				$x += ($rows - 1) % 3;
-				break;
-			case 2:
-				$x = $this->options['columns'] - 1;
-				break;
-		}
-
-		return 30 * intval($rowNum / 3) + $x;
-	}
-
-	private function getRightCodeWord($rowNum, $rows)
-	{
-		$tableID = $rowNum % 3;
-
-		switch($tableID) {
-			case 0:
-				$x = $this->options['columns'] - 1;
-				break;
-			case 1:
-				$x = intval(($rows - 1) / 3);
-				break;
-			case 2:
-				$x = $this->options['securityLevel'] * 3;
-				$x += ($rows - 1) % 3;
-				break;
-		}
-
-		return 30 * intval($rowNum / 3) + $x;
-	}
-
-	private function getPadding($dataCount, $ecCount)
-	{
-		// Total number of data words and error correction words, additionally
-		// reserve 1 code word for the length descriptor
-		$totalCount = $dataCount + $ecCount + 1;
-		$mod = $totalCount % $this->options['columns'];
-
-		if ($mod > 0) {
-			$padCount = $this->options['columns'] - $mod;
-			$padding = array_fill(0, $padCount, 900);
-		} else {
-			$padding = [];
-		}
-
-		return $padding;
+		$pixelGrid = (new DataEncoder($this->options))->encodeData($data);
+		$this->renderer = (new Renderer($pixelGrid, $this->options));
 	}
 }
