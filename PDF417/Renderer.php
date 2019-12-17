0fd724b2b8ebd35f4322f9bcbae26cb06e5f1a69
Momchil Bozhinov <momchil@bojinov.info>
2019-12-17 16:22:31 +0200

    * Fixed indentation

--- PDF417/Renderer.php
+++ PDF417/Renderer.php
@@ -106,69 +106,69 @@ class Renderer
 	}
 
 	public function createSVG()
-    {
-        $height = count($this->pixelGrid);
-        $width = count($this->pixelGrid[0]);
+	{
+		$height = count($this->pixelGrid);
+		$width = count($this->pixelGrid[0]);
 
-        // Apply scaling & aspect ratio
-        $scaleX = $this->options['scale'];
-        $scaleY = $this->options['scale'] * $this->options['ratio'];
+		// Apply scaling & aspect ratio
+		$scaleX = $this->options['scale'];
+		$scaleY = $this->options['scale'] * $this->options['ratio'];
 
-        $width *= $scaleX;
-        $height *= $scaleY;
+		$width *= $scaleX;
+		$height *= $scaleY;
 
-        $doc = $this->createDocument();
+		$doc = $this->createDocument();
 
-        // Root document
-        $svg = $doc->createElement("svg");
-        $svg->setAttribute("height", $height);
-        $svg->setAttribute("width", $width);
-        $svg->setAttribute("version", "1.1");
-        $svg->setAttribute("xmlns", "http://www.w3.org/2000/svg");
+		// Root document
+		$svg = $doc->createElement("svg");
+		$svg->setAttribute("height", $height);
+		$svg->setAttribute("width", $width);
+		$svg->setAttribute("version", "1.1");
+		$svg->setAttribute("xmlns", "http://www.w3.org/2000/svg");
 
-        // Create the group
-        $group = $doc->createElement("g");
-        $group->setAttribute('id', 'barcode');
-        $group->setAttribute('fill', $this->options['color']->toHex());
-        $group->setAttribute('stroke', 'none');
+		// Create the group
+		$group = $doc->createElement("g");
+		$group->setAttribute('id', 'barcode');
+		$group->setAttribute('fill', $this->options['color']->toHex());
+		$group->setAttribute('stroke', 'none');
 
-        // Add barcode elements to group
-        foreach ($this->pixelGrid as $y => $row) {
-            foreach ($row as $x => $item) {
-                if ($item === false) {
-                    continue;
-                }
+		// Add barcode elements to group
+		foreach ($this->pixelGrid as $y => $row) {
+			foreach ($row as $x => $item) {
+				if ($item === false) {
+					continue;
+				}
 
-                $rect = $doc->createElement('rect');
-                $rect->setAttribute("x", $x * $scaleX);
-                $rect->setAttribute("y", $y * $scaleY);
-                $rect->setAttribute("width", $scaleX);
-                $rect->setAttribute("height", $scaleY);
+				$rect = $doc->createElement('rect');
+				$rect->setAttribute("x", $x * $scaleX);
+				$rect->setAttribute("y", $y * $scaleY);
+				$rect->setAttribute("width", $scaleX);
+				$rect->setAttribute("height", $scaleY);
 
-                $group->appendChild($rect);
-            }
-        }
+				$group->appendChild($rect);
+			}
+		}
 
-        $svg->appendChild($group);
-        $doc->appendChild($svg);
+		$svg->appendChild($group);
+		$doc->appendChild($svg);
 
-        return $doc->saveXML();
-    }
+		return $doc->saveXML();
+	}
 
-    /** Creates a DOMDocument for SVG. */
-    private function createDocument()
-    {
-        $impl = new \DOMImplementation();
+	/** Creates a DOMDocument for SVG. */
+	private function createDocument()
+	{
+		$impl = new \DOMImplementation();
 
-        $docType = $impl->createDocumentType(
-            "svg",
-            "-//W3C//DTD SVG 1.1//EN",
-            "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd"
-        );
+		$docType = $impl->createDocumentType(
+		"svg",
+		"-//W3C//DTD SVG 1.1//EN",
+		"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd"
+		);
 
-        $doc = $impl->createDocument(null, null, $docType);
-        $doc->formatOutput = true;
+		$doc = $impl->createDocument(null, null, $docType);
+		$doc->formatOutput = true;
 
-        return $doc;
-    }
+		return $doc;
+	}
 }
